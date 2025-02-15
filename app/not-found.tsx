c002503ef699cc404cf9d2e7aabdb6ef34bdf00a
itsobi <obiobialo@aol.com>
2025-02-14 22:07:47 -0600

    not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-2xl font-bold">Not Found</h2>
+      <p className="text-muted-foreground">
+        The page you are looking for does not exist.
+      </p>
+      <Link
+        href="/"
+        className="border rounded-full py-2 px-4 hover:bg-muted-foreground/10 mt-2"
+      >
+        👈 Return Home
+      </Link>
+    </div>
+  );
+}
