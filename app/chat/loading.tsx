cfc64ad23f58e78174ef8a727a7892118ac2cf9a
itsobi <obiobialo@aol.com>
2025-03-13 00:09:13 -0500

    loading screens

---
+++ app/chat/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingScreen } from '@/components/loading-screen';
+
+export default function ChatLoadingScreen() {
+  return <LoadingScreen message="Gathering chat..." />;
+}
---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingScreen as LoadingScreenComponent } from '@/components/loading-screen';
+
+export default function LoadingScreen() {
+  return <LoadingScreenComponent />;
+}
