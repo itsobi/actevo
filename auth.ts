5e7b32180c9e4756d65c523a374985274c37332a
itsobi <obiobialo@aol.com>
2025-03-13 00:06:35 -0500

    add provider account id to session obj

--- auth.ts
+++ auth.ts
@@ -3,4 +3,18 @@ import Google from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  callbacks: {
+    jwt: async ({ token, account }) => {
+      if (account?.provider === 'google') {
+        token.id = account.providerAccountId;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
 });
